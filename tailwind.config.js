deb8c50bf42ae6b294a8d0ab370e787708d3988c
SnoopyCodeX <extremeclasherph@gmail.com>
2024-01-06 17:50:33 +0800

    refactor: override primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#120D0E",
+        secondary: "#DB924B"
+      }
+    },
   },
   daisyui: {
-    themes: ['coffee']
+    themes: false
   },
   plugins: [require('daisyui')],
 }
