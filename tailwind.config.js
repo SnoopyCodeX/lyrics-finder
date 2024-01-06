c8d4d8357a5b8e5700bd2ed183e185964597a3f2
SnoopyCodeX <extremeclasherph@gmail.com>
2024-01-06 11:21:02 +0800

    fix(theme): dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
